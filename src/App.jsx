7325fa48085ca7cb95ce071e37971b0c5d5ed13b
RahulSubh <rahulsubh1999@gmail.com>
2022-09-14 13:09:52 +0530

    added game history

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 
 import './components/styles/root.scss';
 import { calculateWinner } from './helpers';
 
 const App = () => {
-  const [history, setHistory] = useState([{board : Array(9).fill(null),isXNext : true}]);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
 
-  const [currentMove,setCurrentMove] = useState(0);
+  const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
 
@@ -23,9 +26,8 @@ const App = () => {
     }
 
     setHistory(prev => {
-
       const last = prev[prev.length - 1];
-      const newBoard =  last.board.map((square, pos) => {
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
           return last.isXNext ? 'X' : 'O';
         }
@@ -33,16 +35,21 @@ const App = () => {
         return square;
       });
 
-      return prev.concat({ board : newBoard , isXNext : !last.isXNext })
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
     setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrentMove(move);
   };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
